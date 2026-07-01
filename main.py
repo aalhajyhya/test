ab412099eb089e158410777954bd8f93e9a307c3
aalhajyhya <aalhajyhya@noon.com>
2026-07-01 11:13:00 +0300

    feature add

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ app=FastAPI()
 @app.get("/")
 def HOME():
     return {"message":"this is home"}
+
+@app.get("/nei/{id}")
+def NEIGHBOOR(id):
+    return {"message":"this is neighboor"+str(id)}
+
+@app.get("/friend/{id}")
+def FRIEND(id):
+    return {"message":"this is friend "+str(id)}
+
+@app.get("/feat1/{x}/{y}")
+def feature1(x,y):
+    return {"message":"this is feat1 of "+str(int(x)+int(y))+str(type(x))}
